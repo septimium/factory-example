c0edf47335af65b4ab427e5a6bccd6bc40217893
septimium <septimiutg@gmail.com>
2025-08-01 10:25:01 +0300

    example

---
+++ src/FactoryMethod.kt
@@ -0,0 +1,57 @@
+interface Factory{
+    fun createDevice(): Device
+}
+
+class LaptopFactory : Factory{
+    override fun createDevice(): Device {
+        return Laptop()
+    }
+}
+
+class SmartphoneFactory : Factory{
+    override fun createDevice(): Device {
+        return Smartphone()
+    }
+}
+
+class TabletFactory : Factory{
+    override fun createDevice(): Device {
+        return Tablet()
+    }
+}
+
+abstract class Device(){
+    abstract fun printDetails()
+}
+
+class Laptop: Device(){
+    override fun printDetails() {
+        println("This device is laptop!")
+    }
+}
+
+class Smartphone: Device(){
+    override fun printDetails() {
+        println("This device is a smartphone!")
+    }
+}
+
+class Tablet: Device(){
+    override fun printDetails() {
+        println("This device is a tablet!")
+    }
+}
+
+fun main() {
+    val laptopFactory = LaptopFactory()
+    val laptop = Laptop()
+    laptop.printDetails()
+
+    val tabletFactory = TabletFactory()
+    val tablet = Tablet()
+    tablet.printDetails()
+
+    val smartphoneFactory = SmartphoneFactory()
+    val smartphone = Smartphone()
+    smartphone.printDetails()
+}
